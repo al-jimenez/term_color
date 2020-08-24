c40a5c7f47e5bcd3d4be40e1987a7d25373b4816
Al Jimenez <al.jimenez@mail.com>
2020-08-24 14:50:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# term_color()
+This is a simple function that displays a chart of colors and/or will display a color given a number.
+
+## Use
+Use this and customize it with your changes. I use this as a menu of colors since my old man brain cannot remember the color number for a specific color.
+
+## OS
+Used / created for macOS bash but can be modified for other OS.
+
+## Help menu
+When you execute term_color -? you will get the following:
+![Help menu]()
+
+## With no options
+![without options]()
+
+## Usage Examples
+
+    term_color -?  -  to get the help menu
+
+    term_color <date string> - i.e.: term_color "%I:%M %p, %D" returns >> 10:15 AM, 07/17/20
+
+    Script usage: <variable name>=$(echo "$(term_color fs)")
+
+    Script usage: echo "$(term_color dd/mm/yy)"
---
+++ term_color.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# 📌 html_color_chart() - display color chart & command using tput ANSI commands
+# ------------------------------------------------------------------------------
+term_color() {
+  [[ -z ${1}                ]] && { shift; ${FUNCNAME[0]} l ; return; }
+  [[ "${1}" == "list"       ]] || [[ "${1}" == "l"  ]] && {  for i in $(seq 0 255); do html_color ${i}; done; printf "\n";  return; }
+  [[ "${1}" == "foreground" ]] || [[ "${1}" == "fg" ]] && { shift; tput setaf ${1}; printf "${2}"; tput sgr0; printf "\n"; return; }
+  [[ "${1}" == "background" ]] || [[ "${1}" == "bg" ]] && { shift; tput setab ${1}; printf "${2}"; tput setab 0; printf "\n"; return; }
+  [[ "${1}" == "colors"     ]] || [[ "${1}" == "c"  ]] && { shift;
+      ( x=`tput op` y=`printf %$((${COLUMNS}-6))s`; n="$(tput lines)";
+        for i in {0..256};
+          do { [[ "$i" == "$n" ]] && { n=$(( n + 34 )); pause; };
+               o=00$i; echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;
+          } done;
+      )
+      return;
+  }
+  [[ "${1}" == "?" ]] || [[ "${1}" == "-?" ]] || [[ "${1}" == "help" ]] || [[ "${1}" == "-h" ]] || [[ "${1}" == "?" ]] && {
+    echo -e "
+    PURPOSE: Load color code chart or individual colors
+
+    ${FUNCNAME[0]} <none> | # | <command options>
+
+    OPTIONS:
+      <none>             - display chart of all colors with HTML hex # equivalent
+      #                  - display color's HTML hex # equivalent
+      l  | list          - display color chart of HTML color codes
+      fg | foreground #  - enter set foreground color specified
+      bg | background #  - enter set background color specified
+      ?  | help          - This help.
+    "
+    printf "\t\"tput colors\" available = "; tput colors;
+    printf "\t\"tput setaf #\" to set freground color \n";
+    printf "\t\"tput setab #\" to set background color\n";
+    printf "\t\"tput sgr0\" to turn off all attributes\n";
+    printf "\n"
+    return;
+  }
+  [[ "${1}" != ""   ]] && { html_color ${1}; printf "\n"; return; }                       #
+  }
+  
